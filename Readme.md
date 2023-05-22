93373f29b4f78477d0af2337b733a264811cf0dd
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-05-22 04:16:27 +0100

    Docs and more

--- Readme.md
+++ Readme.md
@@ -4,13 +4,119 @@ A library for creating commnad line apps in rust
 
 ```rust
 fn main(){
-    let mut app : &Fli = Fli::init();
-    app.option("-n --name", |x : &Fli| {
-        println!("say Hello");
+    let mut app : &Fli = Fli::init("my app");
+    app.option("-n --name <>", |x : &Fli| {});
+    app.option("-g --greet", |x : &Fli| {
+        match x.get_values("--name".to_owned()){
+            Ok(option) => {
+                if let Some(v) = option{ println!("Good day {?}", v[0]);}
+            },
+            Err(_) => {},
+        }
     });
-    app.option("-h --hello", |x : &Fli| {
-        println!("say Hello");
-    });
+    // required for the app  to work 
     app.run();
 }
 ```
+
+then run it like this
+```bash
+$ cargo run -- -g -n james
+Good day james
+```
+
+## Getting started
+### Installtion
+```bash
+coming soon
+```
+
+### Import
+```rust
+extern crate fli;
+use fli::Fli;
+fn main(){
+    println!("Happy Coding");
+}
+```
+### Create an App Instance 
+```rust
+fn main(){
+    let mut app = Fli::init("app-name");
+}
+```
+### Adding Options
+
+```rust
+fn main(){
+    let mut app = Fli::init("app-name");
+    app.option("-g --greet", greet);
+    app.option("-n --name, <>" |x|{});
+}
+
+// callback for the greet param
+fn greet(x: &Fli){
+    match x.get_values("--name".to_owned()){
+        Ok(option) => {
+            if let Some(v) = option{ println!("Good day {?}", v[0]);}
+        },
+        Err(_) => {},
+    }
+}
+```
+
+### Run your app
+```rust
+fn main(){
+    //... other code
+    app.run();
+}
+```
+
+### Adding a new Command Set
+You can also add a new command set using the command method
+```rust
+fn main(){
+    let mut app = Fli::init("app-name");
+    let moveCommand = app.command("move");
+    moveCommand.option("-d --dir, <...>", move_file);
+    app.option("-g --greet", greet);
+    app.option("-n --name, <>" |x|{});
+    app.run();
+}
+fn move_file(x){ /*code to move file"*/ }
+fn greet(x){ /*code to greet "*/ }
+```
+Then you would run the command like this
+```shell
+$ cargo run -- move -d .
+file moved successfully in 0.8s
+```
+
+### Doing it in a procedual way
+Like commander.js you can also do it in a procedual way
+
+```rust
+fn main(){
+    //  doing it procedual way
+    let mut app = Fli::init("app-name");
+    let moveCommand = app.command("move");
+    // the [...] means accept optional multiple
+    moveCommand.option("-d --dir, [...]", |x){}; // pass in an empty callback
+    app.option("-g --greet", |x|{});
+    app.option("-n --name, <>" |x|{});
+    app.run();
+
+    if app.is_passed("--greet"){
+        if app.has_a_value("-n"){/* greet person with name*/}
+        else { /* freet without name*/ }
+    }
+}
+```
+
+## Explaining all `app : Fli` methods
+**Note:**
+> All `app : Fli` methods are avaliable as `app : &Fli` methods
+- `app.option(arg_and_data, callback)` : 
+This method takes in 2 param 
+  - First `arg_and_data` : This is a format template of how the avaliable arguments for a command would be being in a format `-a --arg` or `-a --arg, data`
--- src/fli.rs
+++ src/fli.rs
@@ -2,8 +2,11 @@
 use std::{env, collections::HashMap};
 
 pub struct Fli {
+    name:String,
     args : Vec<String>,
     pub args_hash_table: HashMap<String, fn(app : &Self)>,
+    short_hash_table : HashMap<String, String>,
+    cammands_hash_tables : HashMap<String, Fli>
 }
 
 enum FliError{
@@ -12,12 +15,30 @@ enum FliError{
 }
 
 impl Fli {
-    pub fn init() -> Self {
+    pub fn init(name : String) -> Self {
         println!("{:?}", env::args());
         Self {
+            name,
             args: env::args().collect(),
             args_hash_table: HashMap::new(),
+            short_hash_table: HashMap::new(),
+            cammands_hash_tables: HashMap::new(),
         }
+    }
+    pub fn command(&mut self, name: String) -> &mut Fli
+    {
+        let mut args = self.args.clone();
+        args.remove(0);
+        println!("args: {:?}", args);
+        let new_fli = Self {
+            name:name.clone(),
+            args: args,
+            args_hash_table: HashMap::new(),
+            short_hash_table: HashMap::new(),
+            cammands_hash_tables: HashMap::new(),
+        };
+        self.cammands_hash_tables.insert(name.clone(), new_fli);
+        return self.cammands_hash_tables.get_mut(&name).unwrap();
     }
 
     pub fn option(&mut self, key: String, value: fn(app : &Self)) -> &Fli{
@@ -26,76 +47,88 @@ impl Fli {
         if let Some(opts)  = args.get(0){
             options = String::from(opts.to_owned());
         }
-        for i in options.split(" ") {
-            let mut param_type = String::new();
-            if let Some(param_d) = args.get(1){
-                param_type = String::from(param_d.to_owned());
-            }
-            if args.len() > 1 && ["<>", "[]", "<...>", "[...]"].contains(&param_type.trim()) == false {
-                panic!("Error : unknown param type {}", param_type)
-            }
-            let option : String = i.trim().to_owned()+" "+param_type.trim();
-            self.args_hash_table.insert(option.trim().to_owned(), value);
+        let broken_args : Vec<_> = options.split(" ").collect();
+        let short = broken_args[0].trim();
+        let long = broken_args[1].trim();
+        // for i in options.split(" ") {
+        self.short_hash_table.insert(short.to_string(), long.to_string());
+        let mut param_type = String::new();
+        if let Some(param_d) = args.get(1){
+            param_type = String::from(param_d.to_owned());
         }
+        if args.len() > 1 && ["<>", "[]", "<...>", "[...]"].contains(&param_type.trim()) == false {
+            panic!("Error : unknown param type {}", param_type)
+        }
+        let option : String = long.trim().to_owned()+" "+param_type.trim();
+        self.args_hash_table.insert(option.trim().to_owned(), value);
+        // }
         return self
     }
-    pub fn get_command(&mut self, key : String) -> Option<&for<'a> fn(&'a Fli)>
+    pub fn get_params_callback(&mut self, key : String) -> Option<&for<'a> fn(&'a Fli)>
     {
-         if let Some(callback) = self.args_hash_table.get(&key){
+        if let Some(callback) = self.args_hash_table.get(&key){
                 return Some(callback)
         }
         return None;
     }
     pub fn run(&self)  {
-        let mut counter: usize = 0;
         let mut callbacks: Vec<for<'a> fn(&'a Fli)> = vec![];
-        for i in self.args.clone()
+        let mut counter: usize = 0;
+        for mut arg in self.args.clone()
         {
-            if i.starts_with("-") {
-                let find = &i;
-                if let Some(callback) = self.args_hash_table.get(find){
-                    callbacks.push(*callback);
+            // a value based param must start with a - either -- or -
+            if arg.starts_with("-") {
+                arg = self.get_callable_name(arg);
+                for optional_template in ["", "[]", "[...]"]
+                {
+                    // check if it need a required param
+                    let find = &String::from(format!("{arg} {optional_template}"));
+                    if let Some(callback) = self.args_hash_table.get(find.trim()){
+                        callbacks.push(*callback);
+                        // continue;
+                    }
                 }
-                let find = &String::from(format!("{} []", i));
-                if let Some(callback) = self.args_hash_table.get(find){
-                    callbacks.push(*callback);
-                }
-                
-                let find = &String::from(format!("{} <>", i));
-                if let Some(callback) = self.args_hash_table.get(find){
-                    if let Some(value) = self.args.get(counter+1)
-                    {
-                        if value.contains("-") {
-                            panic!("Invalid syntax");
+                for required_template in ["<>", "<...>"]
+                {
+                    // check if it need a required param
+                    let find = &String::from(format!("{arg} {required_template}"));
+                    if let Some(callback) = self.args_hash_table.get(find.trim()){
+                        // make sure a value is passed in else it should show error/help
+                        if !self.has_a_value(arg.trim().to_string())
+                        {
+                            panic!("{}", format!("Why Invalid syntax : {arg}  does not have a value"));
                         }
                         callbacks.push(*callback);
                     }
-                    else{
-                        panic!("Invalid syntax");
-                    }
                 }
-
-                let find = &String::from(format!("{} [...]", i));
-                if let Some(callback) = self.args_hash_table.get(find){
-                    callbacks.push(*callback);
-                }
-                let find = &String::from(format!("{} <...>", i));
-                if let Some(callback) = self.args_hash_table.get(find){
-                    if let Some(value) = self.args.get(counter+1)
-                    {
-                        if value.contains("-") {
-                            panic!("Invalid syntax");
-                        }
-                        callbacks.push(*callback);
-                    }
-                    else{
-                        panic!("Invalid syntax");
-                    }
+            }
+            else{
+                if let Some(command_struct) = self.cammands_hash_tables.get(arg.trim()){
+                    command_struct.run();
+                    break;
                 }
             }
             counter += 1;
         }
         self.run_callbacks(callbacks);
+    }
+    fn has_a_value(&self, arg_name : String) -> bool
+    {
+        let mut counter = 0;
+        let binding = self.get_callable_name(arg_name);
+        let arg_full_name = binding.trim();
+        for arg in &self.args{
+            if  self.get_callable_name(arg.to_string()) == arg_full_name{
+                if let Some(value) = self.args.get(counter+1)
+                {
+                    if !value.contains("-") {
+                        return true;
+                    }
+                }
+            }
+            counter+=1;
+        }
+        return false;
     }
     fn run_callbacks(&self, callbacks : Vec<for<'a> fn(&'a Fli)>)
     {
@@ -104,46 +137,58 @@ impl Fli {
             callback(self)
         }
     }
-
-    pub fn get_values(&self, args: String) -> Result<Option<Vec<String>>, &str>
+    /**
+     * Gets the Long name for a short arg
+     */
+    fn get_callable_name(&self, arg: String) -> String
+    {
+        let mut arg_template: String = String::from(format!("{}", arg));
+        if let Some(long_name) = self.short_hash_table.get(&arg){
+            arg_template = long_name.to_string();
+        }
+        if !arg_template.contains("--"){
+            arg_template = String::from(format!("--{}", arg));
+        }
+        return arg_template;
+    }
+    pub fn get_values(&self, arg: String) -> Result<Option<Vec<String>>, &str>
     {
         let mut values : Vec<String> = vec![];
-        let find = &String::from(format!("{}", args));
+        let mut arg_name: String = self.get_callable_name(arg);
         // if the argument does not need a param then dont return none
-        if let Some(_) = self.args_hash_table.get(find){
+        if let Some(_) = self.args_hash_table.get(&arg_name){
             return Err("Does not expect a value");
         }
         let mut counter = 1;
-        for i in self.args.clone()
+        for mut i in self.args.clone()
         {
-          if i != args{
+          i = self.get_callable_name(i);
+          if i != arg_name{
             counter+=1;
             continue;
           }
-          if i.starts_with("-")  {
-            let find = &String::from(format!("{} []", args));
-            if let Some(_) = self.args_hash_table.get(find){
+            let binding = &String::from(format!("{} []", arg_name));
+            if let Some(_) = self.args_hash_table.get(binding){
                 if let Some(v) = self.args.get(counter) {
                     if v.starts_with("-") {
-                        return Err("No value passed 1");
+                        return Err("No value passed");
                     }
                     values.push(v.to_string());
                     break;
                 }
             }
-            let find = &String::from(format!("{} <>", args));
-            if let Some(_) = self.args_hash_table.get(find){
+            let binding = &String::from(format!("{} <>", arg_name));
+            if let Some(_) = self.args_hash_table.get(binding){
                 if let Some(v) = self.args.get(counter) {
                     if v.starts_with("-") {
-                        println!("{v}, {i}, {args}");
                         return Err("No value Passed 2")
                     }
                     values.push(v.to_string());
                     break;
                 }
             }
-            let find = &String::from(format!("{} [...]", args));
-            if let Some(_) = self.args_hash_table.get(find){
+            let binding = &String::from(format!("{} [...]", arg_name));
+            if let Some(_) = self.args_hash_table.get(binding){
                 if let Some(params) = self.args.get((counter)..self.args.len())
                 {
                     for i in params
@@ -156,8 +201,8 @@ impl Fli {
                     
                 }
             }
-            let find = &String::from(format!("{} <...>", args));
-            if let Some(_) = self.args_hash_table.get(find){
+            let binding = &String::from(format!("{} <...>", arg_name));
+            if let Some(_) = self.args_hash_table.get(binding){
                 if let Some(params) = self.args.get((counter)..self.args.len())
                 {
                     for i in params
@@ -170,10 +215,12 @@ impl Fli {
                     
                 }
             }
-          }
           counter += 1;
         }
-        return Ok(Some(values));
+        if values.len() > 0{
+            return Ok(Some(values));
+        }
+        return Ok(None);
     }
     pub fn is_passed(&self, param : String) -> bool
     {
