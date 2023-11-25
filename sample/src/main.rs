6198b435717a3c3bd80daaef7197072bfe66104e
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-25 21:11:50 +0100

    fli-[feature] [add Fli initialization]

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,5 +1,9 @@
 use crate::fli::Fli;
 
+
+// The commit messaege should in this format "fli-[<commit-reason(feature, bug, etc)>] [<commit-message>]"
+// Example: fli-[feature] [add a new feature]
 fn main() {
+    let app = Fli::init("sample", "A sample app");
     println!("Hello, world!");
 }
