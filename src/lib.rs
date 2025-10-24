a5bad8bd94184457aa7e1e8f6eabc16df9ddc669
codad5 <aniezeoformic@gmail.com>
2025-10-24 20:53:50 +0100

    Add error handling with thiserror and update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-
-#[cfg(not(doctest))]
-pub mod fli;
-pub mod macros;
-pub mod command;
-pub mod option_parser;
 pub mod app;
+pub mod command;
+pub mod error;
+pub mod macros;
+pub mod option_parser;
+pub use error::{FliError, Result};
+pub use app::Fli;
 
-pub use fli::Fli;
 use colored::Colorize;
 #[cfg(test)]
 pub mod tests;
-
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -51,24 +49,4 @@ fn levenshtein_distance(s1: &str, s2: &str) -> usize {
     }
 
     dp[m][n]
-}
-
-fn fli_default_callback(x: &Fli) {
-    let command: Option<String> = x.get_arg_at(1);
-    let command = match command {
-        Some(c) => c,
-        None => "".to_string(),
-    };
-    println!("Command not found: {}", command.bold().red());
-    let err_msg_prefix = match command.len() {
-        0 => "No",
-        _ => "Invalid",
-    };
-    //  if command is not empty print similar command
-    if command.len() <= 0 {
-        x.print_help("No command provided");
-        return;
-    }
-    println!("{0} Command {1} , use the '-h' or '--help' flag to see all command", err_msg_prefix, x.get_app_name().bold().red());
-    x.print_most_similar_commands(command.as_str());
 }
